a23eded7d8c9988813d12c05bd007b2a7f67fec7
bipi <bartosz.pyrz@gmail.com>
2014-04-21 22:16:32 +0200

    Inverted matrix caching using FP

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This code uses caching to avoid recalculating inverse matrices. 
 
-## Write a short comment describing this function
+
+## Function below allows to create a matrix that can then store its inverse in cache.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  i <- NULL
+  set <- function(y) {
+    x <<- y
+    i <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) i <<- inverse
+  getinverse <- function() i
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## Function below allows to get the cached inverse matrix of the object created by makeCacheMatrix
+## or calculate the inverse matrix if necessary.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  i <- x$getinverse()
+  if(!is.null(i)) {
+    message("getting cached data")
+    return(i)
+  }
+  data <- x$get()
+  i <- solve(data, ...)
+  x$setinverse(i)
+  i
 }
